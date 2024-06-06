e0bab1519d5c90543de26b00d84c4251711eaf4a
Timur Daminov <155220892+daminow@users.noreply.github.com>
2024-06-06 20:02:30 +0500

    Delete js directory

--- js/login.js
+++
@@ -1,7 +0,0 @@
-import {createElement, dataToJson, jsonToData, getData, setData, createIconItem, createIcon, createAlertWindow, searchForm, modal} from './data.js'
-setData('pageTags', dataToJson([]))
-document.addEventListener('DOMContentLoaded', () => {
-    setData('lastPage', dataToJson(window.location.href))
-    searchForm()
-    createIcon()
-})
